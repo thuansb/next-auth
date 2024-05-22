ce776f2384bb82c5d68c14075391cbbc04c53b1f
jon <thuan.suba@gmail.com>
2024-05-22 12:52:13 +0700

    ignore tslint

--- packages/next-auth/src/lib/env.ts
+++ packages/next-auth/src/lib/env.ts
@@ -8,6 +8,7 @@ export function reqWithEnvURL(req: NextRequest): NextRequest {
   if (!url) return req
   const { origin: envOrigin } = new URL(url)
   const { href, origin } = req.nextUrl
+  // @ts-ignore
   return new NextRequest.default(href.replace(origin, envOrigin), req)
 }
 
